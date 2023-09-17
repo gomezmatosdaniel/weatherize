d67c25e98862a10cf0376e02a66fb5d08b2bf780
dani <gomezmatosdaniel@gmail.com>
2023-09-17 03:04:15 +0200

    wip

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 const apiKey = '9a31d0e593bebddf01590986be8886f2';
 
 export async function fetchWeather(city) {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=es`;
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
