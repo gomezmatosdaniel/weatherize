968265adeeade05c0704e48f89316c2b2d7cb5c6
Daniel Gomez Matos <web.developer6@roomleader.com>
2023-09-20 10:04:17 +0200

    wip

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,5 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 export async function fetchWeather(city) {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER_API}&lang=es`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.WEATHER_API}&lang=es`;
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
